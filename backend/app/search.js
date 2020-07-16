38a8412c7631627a573951da5781712f80b43a62
yash <yashjohri1200@gmail.com>
2020-07-16 23:09:59 +0530

    Search Added

---
+++ backend/app/search.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Petal = require("../models/petal");
+
+router.get("/search", function (req, res) {
+	console.log("Query: " + req.query.q);
+	Petal.find(
+		{
+			name: {
+				$regex: RegExp(req.query.q),
+				$options: "i",
+			},
+		},
+		{
+			_id: 1,
+			name: 1,
+		},
+		function (err, data) {
+			if (err) {
+				res.sendStatus(404);
+			}
+			res.status(200).send(data);
+		}
+	).limit(15);
+});
+
+module.exports = router;
