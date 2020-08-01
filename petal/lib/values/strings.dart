740a153bb508a6d72beb22b3e8e5bf69b8136509
Yash Johri <yashjohri1200@gmail.com>
2020-08-01 22:06:37 +0530

    ip updated

--- petal/lib/values/strings.dart
+++ petal/lib/values/strings.dart
@@ -1,6 +1,6 @@
 class Strings {
   String appName = 'Petal';
-  String ip = "192.168.0.106:3000";
-  String url = "http://192.168.0.106:3000/petal/";
-  String searchUrl = "http://192.168.0.106:3000/search";
+  static String ip = "192.168.0.106:3000";
+  String url = "http://$ip/petal/";
+  String searchUrl = "http://$ip/search";
 }
