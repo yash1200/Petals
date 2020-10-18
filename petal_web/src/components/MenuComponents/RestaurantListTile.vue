78798f3725b6b251ec312b3a5ca2fb7ecf32e5ad
Yash Johri <yashjohri1200@gmail.com>
2020-10-18 21:38:11 +0530

    Docs updated with macro

---
+++ petal_web/src/components/MenuComponents/RestaurantListTile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="RestaurantListTile">
+    <div class="title">
+      {{ title }}
+    </div>
+    <div class="value">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RestaurantListTile",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.RestaurantListTile {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px 15px 20px;
+}
+
+.title {
+  color: white;
+}
+
+.value {
+  color: white;
+}
+</style>
--- petal_web/src/views/Restaurant.vue
+++ petal_web/src/views/Restaurant.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="Restaurant">
+  <div class="Restaurant" v-if="restaurant != null">
     <RestaurantNavBar />
+    <RestaurantListTile
+      v-if="restaurant != null"
+      :title="'Owner'"
+      :value="restaurant.owner"
+    />
+    <RestaurantListTile
+      v-if="restaurant != null"
+      :title="'Phone'"
+      :value="restaurant.phone"
+    />
+    <ul>
+      <li v-for="(menu, index) in restaurant.menus" :key="index">
+        <button class="collapsible">
+          {{ menu.type }}
+        </button>
+        <RestaurantListTile
+          class="content"
+          v-for="item in menu.items"
+          :key="item.name"
+          :title="item.name"
+          :value="item.price.toString()"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,6 +32,7 @@
 import axios from "axios";
 import * as ResData from "@/model/Restaurant.js";
 import RestaurantNavBar from "@/components/MenuComponents/RestaurantNavBar.vue";
+import RestaurantListTile from "@/components/MenuComponents/RestaurantListTile.vue";
 
 export default {
   name: "Restaurant",
@@ -18,14 +43,16 @@ export default {
   },
   components: {
     RestaurantNavBar,
+    RestaurantListTile,
   },
   methods: {
     getRestaurant() {
       axios
-        .get("http://localhost:3000/petal/" + this.$route.params.id)
+        .get("http://192.168.0.104:3000/petal/" + this.$route.params.id)
         .then((response) => {
           console.log(response.data);
           this.restaurant = ResData.toRestaurant(response.data);
+          console.log(this.restaurant.menus[0].type);
         });
     },
   },
@@ -37,4 +64,50 @@ export default {
 </script>
 
 <style scoped>
+.Restaurant {
+  display: flex;
+  flex-direction: column;
+}
+
+.collapsible {
+  background-color: #303030;
+  color: white;
+  cursor: pointer;
+  padding: 20px 15px 20px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+.active,
+.collapsible:hover {
+  color: green;
+}
+
+.active:after {
+  content: "\2212";
+}
+
+.content {
+  padding: 0 18px;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+  background-color: #f1f1f1;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0px 0px 0px;
+}
+
+.collapsible:after {
+  content: "\002B";
+  color: white;
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
+}
 </style>
